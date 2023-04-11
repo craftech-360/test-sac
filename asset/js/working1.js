f8c5300cc869bd29b2b685829e12a455dfb3ab2d
s-a-c-1-8 <Sac@8teen.in>
2023-04-11 19:29:10 +0530

    cameraaarrr

--- asset/js/working1.js
+++ asset/js/working1.js
@@ -1,54 +1,26 @@
 
 // QR Scanner Code
-// create a new scanner instance
-let scanner = new Instascan.Scanner({
-    video: document.getElementById('preview'),
-    mirror: false,
-    scanPeriod: 5,
-    continuous: true,
-    backgroundScan: false,
-    refractoryPeriod: 5000,
-    // specify the camera source (0 for back camera, 1 for front camera)
-    facingMode: "environment" 
-  });
-  
-  // add a listener for when a code is scanned
-  scanner.addListener('scan', function (content) {
-    console.log(content);
-  });
-  
-  // start scanning
-  Instascan.Camera.getCameras().then(function (cameras) {
-    if (cameras.length > 0) {
-      scanner.start(cameras[0]);
-    } else {
-      console.error('No cameras found.');
+const socket = io();
+let scanner = new Instascan.Scanner(
+    {
+        video: document.getElementById('preview')
     }
-  }).catch(function (e) {
-    console.error(e);
-  });
-  
-// const socket = io();
-// let scanner = new Instascan.Scanner(
-//     {
-//         video: document.getElementById('preview')
-//     }
-// );
-// var a = [];
-// scanner.addListener('scan', function(content) {
-//    console.log(content)
-//    var a = content;
-//     socket.emit('getUserOne',(content));   
-// });
+);
+var a = [];
+scanner.addListener('scan', function(content) {
+   console.log(content)
+   var a = content;
+    socket.emit('getUserOne',(content));   
+});
 
-// Instascan.Camera.getCameras().then(cameras => 
-// {
-//     if(cameras.length > 0){
-//         scanner.start(cameras[1]);
-//     } else {
-//         console.error("camera not started");
-//     }
-// });
+Instascan.Camera.getCameras().then(cameras => 
+{
+    if(cameras.length > 0){
+        scanner.start(cameras[0]);
+    } else {
+        console.error("camera not started");
+    }
+});
    
  // Start Button
  function getStarted() {
